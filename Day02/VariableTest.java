f7ce1dfcd4f42015a0cac891b401f9e8fd4f1609
zzq1096279113 <1096279113@qq.com>
2020-05-31 08:39:50 +0800

    第一天

---
+++ Day02/VariableTest.java
@@ -0,0 +1,14 @@
+class VariableTest{
+	public static void main(String[] args) {
+		
+		int myAge = 18;	//变量的定义
+		int myNumber;			//变量的声明
+		myNumber = 2017310807;	//变量的赋值
+		long a1 = 65465456L;
+
+		System.out.println(myAge);
+		System.out.println(myNumber);
+		System.out.print(a1);
+
+	}
+}
