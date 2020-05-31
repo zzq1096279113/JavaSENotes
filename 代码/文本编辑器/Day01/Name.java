e70d31a1a06133c79fc9800337f83e8e84e32d28
zzq1096279113 <1096279113@qq.com>
2020-05-31 12:19:39 +0800

    完

---
+++ 代码/文本编辑器/Day01/Name.java
@@ -0,0 +1,8 @@
+public class Name{
+	public static void main(String[] args) {
+		System.out.println("姓名:习大大");
+		System.out.println();
+		System.out.println("性别:男");
+		System.out.println("家庭住址:中南海");
+	}
+}
