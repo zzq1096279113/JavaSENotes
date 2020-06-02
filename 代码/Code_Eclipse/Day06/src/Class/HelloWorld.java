1de67ad79e3b286aa931b18cb339f70f76d75d1f
zzq1096279113 <1096279113@qq.com>
2020-06-03 07:50:43 +0800

    第6天

---
+++ 代码/Code_Eclipse/Day06/src/Class/HelloWorld.java
@@ -0,0 +1,13 @@
+package Class;
+/**
+ * 
+ * @Descripion 输出Hello World
+ * @author zzq
+ * @version
+ * @date 2020年5月6日下午5:51:11
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
