f034b53f200b09f2b4c1b09be214ee7a9b9afaa0
zzq <1096279113@qq.com>
2020-08-08 17:19:27 +0800

    添加了文件压缩与解压的使用

--- 数据结构与算法/Algorithm/src/huffmancode/HuffmanCode.java
+++ 数据结构与算法/Algorithm/src/huffmancode/HuffmanCode.java
@@ -1,10 +1,7 @@
 package huffmancode;
 
-import java.util.*;
-
-
 /**
- * @Description 哈夫曼编码
+ * @Description 哈夫曼编码压缩与解压
  * @ClassName HuffmanCode
  * @Author zzq
  * @Date 2020/8/7 10:35
@@ -12,10 +9,19 @@ import java.util.*;
 public class HuffmanCode {
 
     public static void main(String[] args) {
-        String content = "i like like like java do you like a java";
-        byte[] bytes = content.getBytes();
-        byte[] zip = Huffman.huffmanZip(bytes);
-        System.out.println(Arrays.toString(zip));
+//        String content = "i like like like java do you like a java";
+//        byte[] bytes = content.getBytes();
+//        byte[] zip = Huffman.huffmanZip(bytes);
+//        byte[] decode = Huffman.decode(zip,Huffman.huffmanCodes);
+//        System.out.println(new String(decode));
+//        String srcFile = "F:\\Java数据结构和算法.pptx";
+//        String dstFile = "F:\\Java数据结构和算法.zip";
+//        Huffman.zipFile(srcFile,dstFile);
+//        System.out.println("压缩完毕");
+        String srcFile = "F:\\Java数据结构和算法.zip";
+        String dstFile = "F:\\Java数据结构和算法.pptx";
+        Huffman.decodeFile(srcFile, dstFile);
+        System.out.println("解压完毕");
     }
 }
 
