a9b2c927ad59abcc69024af88b6c2b61e352a375
zzq <1096279113@qq.com>
2020-08-12 18:32:26 +0800

    最小生成树：Kruskal算法

---
+++ 数据结构与算法/Algorithm/src/mintree/KruskalAlgorithm.java
@@ -0,0 +1,13 @@
+package mintree;
+
+/**
+ * @Description 最小生成树：Kruskal算法
+ * @ClassName KruskalAlgorithm
+ * @Author zzq
+ * @Date 2020/8/12 18:27
+ */
+public class KruskalAlgorithm {
+    public static void main(String[] args) {
+        
+    }
+}
