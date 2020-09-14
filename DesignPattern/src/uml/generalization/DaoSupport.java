ef8d595699815d04c6bcf31031714d6b26c365d3
zzq <1096279113@qq.com>
2020-09-14 19:07:12 +0800

    泛化（继承）

---
+++ DesignPattern/src/uml/generalization/DaoSupport.java
@@ -0,0 +1,15 @@
+package uml.generalization;
+
+/**
+ * @Description 泛化（继承）
+ * @ClassName DaoSupport
+ * @Author zzq
+ * @Date 2020/9/13 11:43
+ */
+public abstract class DaoSupport {
+    public void save(Object entity) {
+    }
+
+    public void delete(Object id) {
+    }
+}
---
+++ DesignPattern/src/uml/generalization/PersonServiceBean.java
@@ -0,0 +1,11 @@
+package uml.generalization;
+
+/**
+ * @Description 泛化（继承）
+ * @ClassName PersonServiceBean
+ * @Author zzq
+ * @Date 2020/9/13 11:43
+ */
+public class PersonServiceBean extends DaoSupport {
+
+}
